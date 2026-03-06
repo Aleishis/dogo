2198b6a9b1bf93e7c708ab815493ab9de753534c
aleishis <mecedesval@gmail.com>
2026-03-06 09:23:30 -0700

    Se agrega funcionalidad al app.py, ahora la app se puede correr

--- app.py
+++ app.py
@@ -4,5 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.hmtl')
+    return render_template('index.html')
 
+if __name__ == '__main__':
+    app.run(port=5069, host='0.0.0.0')
